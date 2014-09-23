83579739d1fbe20953641b2a817e8d3e053ec6b8
connorclockwise <connorclockwise@gmail.com>
2014-09-22 22:57:28 -0400

    importing custom appointment calendar and using a date picker instead of stock wpf calendar

--- Inventory.WPF/PlannerControl.xaml.cs
+++ Inventory.WPF/PlannerControl.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Text.RegularExpressions;
+using MonthCalendar;
 
 
 
@@ -29,66 +30,77 @@ namespace Inventory.WPF
 
         private List<PlannerItemModel> allPlannerItems;
         private List<PlannerItemModel> selectedPlannerItems;
+        private List<Appointment> _myAppointmentsList;
         private DateTime tempDateTime;
+
 
         public PlannerControl()
         {
             InitializeComponent();
-            this.DataContext = this;
             allPlannerItems = new List<PlannerItemModel>();
             selectedPlannerItems = new List<PlannerItemModel>();
-            plannerCalendar.SelectedDate = DateTime.Today;
-            //plannerCalendar.CalendarDayButtonStyle;
+            _myAppointmentsList = new List<Appointment>();
+            PlannerDatePicker.SelectedDate = DateTime.Today;
+            Loaded += Planner_Loaded;
             initSources();
+            this.DataContext = this;
             
         }
 
         private void initSources()
         {
             RecipeCombo.ItemsSource = getRecipes();
-            PlannerGrid.ItemsSource = getPlannerItems();
-            PlannerGrid.Items.Refresh();
+            //PlannerGrid.ItemsSource = getPlannerItems();
+            //PlannerGrid.Items.Refresh();
         }
 
-        private void plannerCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        private void SetAppointments()
         {
-            updateRecipesByDate();
-            //Console.WriteLine("Something at all");
-            //Console.WriteLine(plannerCalendar.DisplayDate);
+            //-- Use whatever function you want to load the MonthAppointments list, I happen to have a list filled by linq that has
+            //   many (possibly the past several years) of them loaded, so i filter to only pass the ones showing up in the displayed
+            //   month.  Note that the "setter" for MonthAppointments also triggers a redraw of the display.
+            AptCalendar.MonthAppointments = _myAppointmentsList.FindAll(new System.Predicate<Appointment>((Appointment apt) => apt.StartTime != null && Convert.ToDateTime(apt.StartTime).Month == this.AptCalendar.DisplayStartDate.Month && Convert.ToDateTime(apt.StartTime).Year == this.AptCalendar.DisplayStartDate.Year));
         }
 
-        private void recipeGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            Console.Write(e.ToString());
+        private void Planner_Loaded(Object sender, EventArgs eventArgs){
+            Random rand = new Random(System.DateTime.Now.Second);
+
+            for (int i = 1; i <= 50; i++)
+            {
+                Appointment apt = new Appointment();
+                apt.AppointmentID = i;
+                apt.StartTime = new System.DateTime(System.DateTime.Now.Year, rand.Next(1, 12), rand.Next(1, System.DateTime.DaysInMonth(System.DateTime.Now.Year, System.DateTime.Now.Month)));
+                apt.EndTime = apt.StartTime;
+                apt.Subject = "Random apt, blah blah";
+                _myAppointmentsList.Add(apt);
+            }
+
+            SetAppointments();
         }
+
+        //private void plannerCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    updateRecipesByDate();
+        //    //Console.WriteLine("Something at all");
+        //    //Console.WriteLine(plannerCalendar.DisplayDate);
+        //}
+
+        //private void recipeGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    Console.Write(e.ToString());
+        //}
 
         private void btnSubmit_Click(object sender, RoutedEventArgs e)
         {
             saveRecipe();
         }
 
-        private void Insert_Recipe_Click(object sender, RoutedEventArgs e)
-        {
-            tempDateTime = plannerCalendar.SelectedDate ?? System.DateTime.Today;
-            tempDateTime = tempDateTime.Date;
-            //Console.WriteLine("Before change " + tempDateTime);
-            MainPlannerGrid.Visibility = Visibility.Collapsed;
-            PlannerItemGrid.Visibility = Visibility.Visible;
-            //Console.WriteLine("After change " + tempDateTime);
-        }
-
-        private void Edit_Recipe_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-
         private void updateRecipesByDate()
         {
             if (allPlannerItems != null && allPlannerItems.Count > 0)
             {
-                tempDateTime = plannerCalendar.SelectedDate ?? default(DateTime);
-                selectedPlannerItems = allPlannerItems.FindAll(x => x.Date.CompareTo(plannerCalendar.SelectedDate) == 0);
+                tempDateTime = PlannerDatePicker.SelectedDate ?? default(DateTime);
+                selectedPlannerItems = allPlannerItems.FindAll(x => x.Date.CompareTo(PlannerDatePicker.SelectedDate) == 0);
                 initSources();
             }
         }
@@ -107,11 +119,11 @@ namespace Inventory.WPF
 
             //Console.WriteLine("Here's our date" + plannerItemDate);
 
-            MainPlannerGrid.Visibility = Visibility.Visible;
-            PlannerItemGrid.Visibility = Visibility.Collapsed;
+            //MainPlannerGrid.Visibility = Visibility.Visible;
+            //PlannerItemGrid.Visibility = Visibility.Collapsed;
 
             initSources();
-            updateRecipesByDate();
+            //updateRecipesByDate();
         }
 
         //Domain Calls
@@ -126,7 +138,7 @@ namespace Inventory.WPF
         {
             //IRecipeManager manager = ManagerFactory.GetRecipeManager();
             //return manager.getRecipes();
-            return selectedPlannerItems;
+            return allPlannerItems;
         }
 
         
