24b4d9405360ba0c08b318dcb93f477a6d9b98d0
Jonathan Egbert <jonathan.egbert@angryhangar.com>
2014-11-28 13:45:35 -0500

    working on Addrecipe control

---
+++ Inventory.WPF/AddRecipeControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Inventory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Inventory.WPF
+{
+    public partial class AddRecipeControl : UserControl
+    {
+
+        public event RoutedEventHandler Click;
+
+        public AddRecipeControl()
+        {
+            InitializeComponent();
+        }
+
+        void onButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (this.Click != null)
+                this.Click(this, e);
+        }
+    }
+}
