e475d43062070d8e3217072ec39f2fcf1a75b56f
Jonathan Egbert <jonathan.egbert@angryhangar.com>
2014-12-03 12:32:30 -0500

    Why are chenges missing

--- Inventory.WPF/AddRecipeControl.xaml.cs
+++ Inventory.WPF/AddRecipeControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Inventory.Models;
+﻿using Inventory.Data.Interfaces;
+using Inventory.Managers.Factory;
+using Inventory.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,20 +18,65 @@ using System.Windows.Shapes;
 
 namespace Inventory.WPF
 {
+    /// <summary>
+    /// Interaction logic for AddRecipeControl.xaml
+    /// </summary>
     public partial class AddRecipeControl : UserControl
     {
-
-        public event RoutedEventHandler Click;
-
         public AddRecipeControl()
         {
             InitializeComponent();
         }
 
-        void onButtonClick(object sender, RoutedEventArgs e)
-        {
-            if (this.Click != null)
-                this.Click(this, e);
-        }
+        //private void btnSaveRecipe_Click(object sender, RoutedEventArgs e)
+        //{
+        //    //Validate
+        //    if ((String.IsNullOrEmpty(txtName.Text.Trim())) || (String.IsNullOrEmpty(txtDirections.Text.Trim())) || (recipeItems.Count < 1))
+        //    {
+        //        lblError.Content = "Invalid Recipe, Unable to save";
+        //        return;
+        //    }
+        //    //--------------------------------------
+        //    SaveRecipe();
+        //    clearAddRecipe();
+        //}
+
+        //private void SaveRecipe()
+        //{
+        //    RecipeModel recipeItem = new RecipeModel();
+
+        //    recipeItem.Name = txtName.Text.Trim();
+        //    recipeItem.Description = txtDescription.Text.Trim();
+        //    recipeItem.Directions = txtDirections.Text.Trim();
+        //    recipeItem.Tags = txtTags.Text.Trim();
+        //    IRecipeManager manager = ManagerFactory.GetRecipeManager();
+        //    int rid = manager.SaveRecipe(recipeItem, false);
+        //    foreach (String s in newtags)
+        //    {
+        //        manager.SaveTag(s.Substring(0, 1).ToUpper() + s.Substring(1).ToLower());
+        //    }
+        //    foreach (TempRecipeItemModel m in recipeItems)
+        //    {
+        //        m.IngredientID = getIngredientId(m.IngredientName);
+        //        m.MeasureID = getMeasureID(m.MeasureName);
+        //        manager.SaveRecipeItem(rid, m);
+        //    }
+
+
+        //}
+
+        //private void clearAddRecipe()
+        //{
+        //    lblError.Content = "";
+        //    txtName.Text = "";
+        //    txtDescription.Text = "";
+        //    txtDirections.Text = "";
+        //    txtIngredient.Text = "";
+        //    txtQuantity.Text = "";
+        //    txtTags.Text = "";
+        //    recipeItems.Clear();
+        //    instructions = "";
+        //    newtags.Clear();
+        //}
     }
 }
